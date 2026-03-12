82207313a0e0b159ec70c208aa0444ed4d9a854f
edilayehu <edilayehu@eaglelionsystems.com>
2026-03-12 05:15:34 +0300

    Initial Commit

---
+++ Elvis/ElvisApp.swift
@@ -0,0 +1,17 @@
+//
+//  ElvisApp.swift
+//  Elvis
+//
+//  Created by Edil on 12/03/2026.
+//
+
+import SwiftUI
+
+@main
+struct ElvisApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
